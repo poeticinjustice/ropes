0e270b753cecaf5c5f40ca39882dfd1871ccd59a
poeticinjustice <poeticinjustice@gmail.com>
2021-04-11 21:01:22 -0400

    add bootstrap, header, and footer

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href='/'>Ropes</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <Nav.Link href='/mypage'>
+                <i className='fas fa-scroll'></i> My Page
+              </Nav.Link>
+              <Nav.Link href='/login'>
+                <i class='fas fa-user'></i> Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
+
+export default Header
