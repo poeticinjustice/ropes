ffdec0197864615401b09e4dbb914ebbda4c2bdb
poeticinjustice <poeticinjustice@gmail.com>
2021-05-10 22:22:36 -0400

    Create user login reducer & action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,13 +5,21 @@ import {
   personListReducer,
   personDetailsReducer,
 } from './reducers/personReducers'
+import { userLoginReducer } from './reducers/userReducers'
 
 const reducer = combineReducers({
   personList: personListReducer,
   personDetails: personDetailsReducer,
+  userLogin: userLoginReducer,
 })
 
-const initialState = {}
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null
+
+const initialState = {
+  userLogin: { userInfo: userInfoFromStorage },
+}
 
 const middleware = [thunk]
 
