83958595584c18f23ac15ab7de6ccdfef5f5b271
poeticinjustice <poeticinjustice@gmail.com>
2021-11-17 18:14:41 -0500

    fix image upload to use aws

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,47 +1,41 @@
 import express from 'express'
 import multer from 'multer'
-const router = express.Router()
-// import { protect, admin } from '../middleware/authMiddleware.js'
-
 import multerS3 from 'multer-s3'
 import aws from 'aws-sdk'
 
-//multer s3
-const s3 = new aws.S3()
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename(req, file, cb) {
+    cb(null, `${Date.now()}.jpg`)
+  },
+})
+
+const upload = multer({ storage })
+
+const router = express.Router()
+
+router.post('/', upload.single('image'), (req, res) => {
+  res.send(`/${req.file.path}`)
+})
+
 aws.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  accessSecretKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: 'us-east-1',
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 })
-
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-    cb(null, true)
-  } else {
-    cb(new Error('Invalid file type, only JPEG and PNG is allowed!'), false)
-  }
-}
-
-const upload = multer({
-  fileFilter,
-  storage: multerS3({
-    acl: 'public-read',
-    s3: s3,
-    bucket: `ropes-app`,
-    metadata: function (req, file, cb) {
-      cb(null, { fieldName: 'TESTING_METADATA' })
-    },
-    key: function (req, file, cb) {
-      cb(null, Date.now().toString())
-    },
-  }),
+const s3 = new aws.S3()
+const storageS3 = multerS3({
+  s3,
+  bucket: 'ropes-app',
+  acl: 'public-read',
+  contentType: multerS3.AUTO_CONTENT_TYPE,
+  key(req, file, cb) {
+    cb(null, file.originalname)
+  },
 })
-
-const returnImageUrl = (req, res) => {
-  return res.send(`${req.file.location}`)
-}
-
-// router.route('/').post(protect, admin, upload.single('image'), returnImageUrl)
-router.route('/').post(upload.single('image'), returnImageUrl)
-
+const uploadS3 = multer({ storage: storageS3 })
+router.post('/s3', uploadS3.single('image'), (req, res) => {
+  res.send(req.file.location)
+})
 export default router
